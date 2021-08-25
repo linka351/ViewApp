4e73ea1eb212a58a027ca28c118bce82170bf456
Kamil Linka <linka351@gmail.com>
2021-08-25 11:50:17 +0200

    add_form

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,9 +4,17 @@ const Search = () => {
     return (
         <div className={"container"}>
             <div className={"main"}>
-                <h2>Witaj Kamil</h2>
+                <h2>Witaj Name!</h2>
+                <div className={"search_movie"}>
+                    <form className={"search_movie_form"}>
+                        <label className={"search_movie_label"}>Wyszukaj film</label>
+                        <input className={"search_movie_input"} type={"text"} name={"query"} placeholder={"Nazwa filmu"}/>
+                        <button className={"search_movie_button"} type={"submit"}>Wyszukaj</button>
+                    </form>
+                </div>
             </div>
         </div>
+
     )
 }
 
