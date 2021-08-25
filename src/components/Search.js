9134517f87b229f1521d0ade94e5b0ce8d3dbcac
Kamil Linka <linka351@gmail.com>
2021-08-25 16:18:53 +0200

    add_styles_form

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,13 +4,15 @@ const Search = () => {
     return (
         <div className={"container"}>
             <div className={"main"}>
-                <h2>Witaj Name!</h2>
+                <h2>Witaj Name! w swojej wyszukiwarce filmów</h2>
                 <div className={"search_movie"}>
-                    <form className={"search_movie_form"}>
-                        <label className={"search_movie_label"}>Wyszukaj film</label>
-                        <input className={"search_movie_input"} type={"text"} name={"query"} placeholder={"Nazwa filmu"}/>
-                        <button className={"search_movie_button"} type={"submit"}>Wyszukaj</button>
-                    </form>
+                        <div className={"search_movie_container"}>
+                            <form className={"search_movie_form"}>
+                                <label className={"search_movie_label"}>Wyszukaj film</label>
+                                <input className={"search_movie_input"} type={"text"} name={"query"} placeholder={"Nazwa filmu"}/>
+                                <button className={"search_movie_button"} type={"submit"}>Szukaj</button>
+                            </form>
+                        </div>
                 </div>
             </div>
         </div>
