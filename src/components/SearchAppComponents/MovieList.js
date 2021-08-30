0b7bfb0c7d66bcf4d0a9b8f023c7e903df5cecf3
Kamil Linka <linka351@gmail.com>
2021-08-30 12:15:51 +0200

    add_buttons

--- src/components/SearchAppComponents/MovieList.js
+++ src/components/SearchAppComponents/MovieList.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { FaStar } from 'react-icons/fa';
+import { FaPlusCircle } from 'react-icons/fa'
+
 
 const MovieList = ({movies}) => {
     return (
@@ -7,8 +10,19 @@ const MovieList = ({movies}) => {
             {movies.map((movie, index) => (
                 <div>
                     <img className={"image_box"} src={movie.Poster} alt={"movie"}/>
+                    <div className={"image_button"}>
+                        <div className={"left_image_button"}>
+                            <FaPlusCircle/>
+                            <FaPlusCircle/>
+                        </div>
+                        <div className={"right_image_button"}>
+                            <FaStar/>
+                        </div>
+                    </div>
                 </div>
+
             ))}
+
             </div>
         </>
     )
