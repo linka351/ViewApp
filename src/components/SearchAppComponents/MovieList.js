846e6085f3da178e87c8a5b6a6c8feea068a2bdd
Kamil Linka <linka351@gmail.com>
2021-08-30 17:41:45 +0200

    add_tooltip

--- src/components/SearchAppComponents/MovieList.js
+++ src/components/SearchAppComponents/MovieList.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { FaStar } from 'react-icons/fa';
-import { FaPlusCircle } from 'react-icons/fa'
+import { FaStar, FaPlusCircle, FaClock } from 'react-icons/fa';
+import ReactTooltip from "react-tooltip";
+
+
 
 
 const MovieList = ({movies}) => {
+
     return (
         <>
             <div className={"image_container"}>
@@ -11,13 +14,22 @@ const MovieList = ({movies}) => {
                 <div>
                     <img className={"image_box"} src={movie.Poster} alt={"movie"}/>
                     <div className={"image_button"}>
-                        <div className={"left_image_button"}>
-                            <FaPlusCircle/>
-                            <FaPlusCircle/>
+                        <div className={"left_image_button"} >
+                            <button data-tip data-for="add_to_watch"><FaPlusCircle/></button>
+                            <button data-tip data-for="add_to_watched"><FaPlusCircle/></button>
                         </div>
+                        <ReactTooltip id="add_to_watch" place="top" effect="solid">
+                            Dodaj film do obejrzanych
+                        </ReactTooltip>
+                        <ReactTooltip id="add_to_watched" place="top" effect="solid">
+                            Dodaj film do obejrzenia
+                        </ReactTooltip>
                         <div className={"right_image_button"}>
-                            <FaStar/>
+                            <button data-tip data-for="add_to_favourites"><FaStar/></button>
                         </div>
+                        <ReactTooltip id="add_to_favourites" place="top" effect="solid">
+                            Dodaj do ulubionych
+                        </ReactTooltip>
                     </div>
                 </div>
 
